d3a3eab56dd302fd023509acfe0e2a09fa47e1ec
Asakuri <phungtanphat23@gmail.com>
2023-10-15 00:59:03 +0700

    Fix middleware public path, update db scheme (add Comment likes, change user id to 32, index banTin on MaBanTin).

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/auth/dang-nhap", "/auth/dang-ki", "/bantin/:path", "/danhmuc/:path"],
+	publicRoutes: ["/", "/auth/dang-nhap", "/auth/dang-ki", "/bantin/:path", "/danhmuc/:path", "/api/trpc/:path", "/api/clerk/:path"],
 });
 
 export const config = {
